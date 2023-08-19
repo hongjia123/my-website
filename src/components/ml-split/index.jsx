3db4056c7d4fb59ba1251168248b88002a46ad92
merlin <1023982818@qq.com>
2023-08-20 03:50:58 +0800

    目录封装调试

--- src/components/ml-split/index.jsx
+++ src/components/ml-split/index.jsx
@@ -17,109 +17,7 @@ const Split = {
       middle: {}, // 内容parentNode
     });
 
-    // let firstDir = {
-    //   currIndex: 0, //当前目录索引
-    //   // 从一级目录离开二级
-    //   leaveSecond: function (dir, index, setHash) {
-    //     if (dir[index].childDir.length != 0) {
-    //       dir[index].childDir[secondDir.currIndex].dirActive = false;
-    //     }
-    //     setHash && setHash(dir);
-    //   },
-    //   // 从一级目录进入二级
-    //   enterSecond: function (dir, index, setHash) {
-    //     if (dir[index].childDir.length != 0) {
-    //       dir[index].childDir[secondDir.currIndex].dirActive = true;
-    //     }
-    //     setHash && setHash(dir);
-    //   }
-    // }; // 一级目录属性对象
-    // let secondDir = {
-    //   currIndex: 0 // 当前目录索引
-    // }; // 二级目录属性对象
-
-    // /**
-    //  * @param {Object} e 当前节点滚动属性对象
-    //  * @param {Array<Object>} dir 一级目录对象或者二级目录对象
-    //  * @param {Object} dirPropretyObj 目录属性对象
-    //  */
-    // const scrollDir = function (e, dir, dirPropretyObj) {
-    //   if (!dir || dir.length == 0) return
-    //   const { currIndex, leaveSecond, enterSecond } = dirPropretyObj;
-    //   if (!e.target.scrollTop) {
-    //     dir[currIndex].dirActive = false;
-    //   }
-    //   if (e.target.scrollTop > dir[currIndex].offsetTop - 100) {
-    //     dir[0].dirActive = !currIndex;
-    //     // 滚动到下一个目录
-    //     if ((currIndex < dir.length - 1) && (e.target.scrollTop > dir[currIndex + 1].offsetTop - 100)) {
-    //       leaveSecond && leaveSecond(dir, currIndex, setHash); // 离开二级目录
-    //       dirPropretyObj.currIndex++;
-    //       dir[currIndex + 1].dirActive = true;
-    //       dir[currIndex].dirActive = false;
-
-    //     }
-    //   } else {
-    //     if (currIndex) {
-    //       dir[currIndex - 1].dirActive = true;
-    //       dir[currIndex].dirActive = false;
-    //       dirPropretyObj.currIndex--;
-    //       enterSecond && enterSecond(dir, dirPropretyObj.currIndex,setHash);
-    //     } else {
-    //       dir[currIndex].dirActive = false;
-    //     }
-    //   }
-    //   // 开始滚动二级目录
-    //   scrollDir(e, dir[currIndex].childDir, secondDir);
-
-
-
-    // };
-    // const setHash = (dir) => {
-    //   // 设置hash
-    //   if (!id || id !== dir.find(item => item.dirActive)?.id) {
-    //     id = dir.find(item => item.dirActive)?.id;
-    //     location.href = location.href.replace(/(\.html)(#\w+|$)$/, `$1${id ? ('#' + id) : ''}`);
-    //   }
-    // }
-
-    // const setArray = (collect) => {
-    //   return [].slice.call(collect)
-    // }
-    // // 设置目录节点
-    // const setDirNode = (node) => {
-    //   const dirNode = setArray(document.getElementsByTagName('h3'));//一级标题node
-    //   state.directory = dirNode.map(item => {
-    //     const childDir = [];
-    //     setArray(item.parentNode.getElementsByTagName('h4')).map((node, index) => {
-    //       childDir.push({
-    //         name: node.innerText,
-    //         offsetTop: node.offsetTop,
-    //       })
-
-    //     })
-    //     return {
-    //       name: item.innerHTML,
-    //       id: item.id,
-    //       offsetTop: item.offsetTop,
-    //       dirActive: item.id == route.hash.replace('#', ''),
-    //       childDir: childDir || ''
-    //     }
-    //   })
-    //   const offsetTop = route.hash && document.querySelector(route.hash).offsetTop - 80;
-    //   const fn = () => {
-    //     const scrollTop = document.querySelector('.middle-content-container').scrollTop;
-    //     const isspeed = offsetTop / 8;
-    //     document.querySelector('.middle-content-container').scrollTop = (scrollTop + isspeed) > offsetTop ? offsetTop : (scrollTop + isspeed);
-    //     if (scrollTop < offsetTop) {
-    //       raf = requestAnimationFrame(fn);
-    //     } else {
-    //       cancelAnimationFrame(raf);
-    //     }
-    //   };
-    //   raf = requestAnimationFrame(fn);
-    // };
-    onMounted(() => {
+    onMounted(async () => {
       const left = document.querySelector('.left-title-nav');
       const splitline = document.querySelector('.split-line');
       state.middle = document.querySelector('.middle-content-container');
@@ -152,16 +50,16 @@ const Split = {
         container: '.middle-content-container',
         firstDirNode: 'h3',
         secondDirNode: 'h4',
-        isSetHash:false
+        isSetHash: true
       });
-      myDir.observe().then((res) => {
-        state.directory = myDir.directory.value;
-      })
+
+      state.directory = await myDir.observe()
+
       // 滚动激活当前标题目录
       state.middle.addEventListener('scroll',
         function (e) {
           document.querySelector('.right-container').scrollTop = e.target.scrollTop;
-          myDir.scrollDir(e);
+          myDir.scrollDir(e, state.directory);
         }
       );
 
@@ -188,13 +86,13 @@ const Split = {
               state.directory.map(item => {
                 return (
                   <div >
-                    <span style={{ fontSize: '14px', margin: '2px 0', fontWeight: 'bold' }} class={{ 'dir_active': item.dirActive }}>
+                    <span style={{ fontSize: '14px', margin: '2px 0', fontWeight: 'bold',cursor:'pointer' }} class={{ 'dir_active': item.dirActive }}>
                       {item.name}
                     </span>
                     {
                       item.childDir?.map(i => {
                         return (
-                          <span style={{ fontSize: '12px', margin: '2px 0' }} class={{ 'second_active': i.dirActive }}>{i.name}</span>
+                          <span style={{ fontSize: '12px', margin: '2px 0',cursor:'pointer' }} class={{ 'second_active': i.dirActive }}>{i.name}</span>
                         )
                       })
                     }
--- src/components/ml-split/util.js
+++ src/components/ml-split/util.js
@@ -6,52 +6,63 @@ export const setArray = (collect) => {
 };
 class SetDirectory {
     constructor(option) {
+        // 离开二级目录
+        let leaveSecond = (directory, index) => {
+            if (directory[index].childDir.length != 0) {
+                console.log(this.secondDir);
+                directory[index].childDir[this.secondDir.currIndex].dirActive = false;
+            }
+            console.log(this);
+        };
+        // 进入二级目录
+        let enterSecond = (directory, index) => {
+            if (directory[index].childDir.length != 0) {
+                directory[index].childDir[this.secondDir.currIndex].dirActive = true;
+            }
+        };
         this.directory = ref([]);
         this.isSetHash = option.isSetHash;
         this.currIndex = option.currIndex; // 设置当前目录索引
-        this.container = document.querySelector(option.container); // 目录文本容器
         // 一级目录属性对象
         this.firstDir = {
             currIndex: 0, //当前目录索引
-            dirNode: document.getElementsByTagName(option.firstDirNode),// 一级目录对应文本元素节点
+            contentNode: '',// 一级目录对应文本元素节点
             // 从一级目录离开二级
-            leaveSecond: this.leaveSecond,
+            leaveSecond: option.secondDirNode && leaveSecond,
             // 从一级目录进入二级
-            enterSecond: this.enterSecond
+            enterSecond: option.secondDirNode && enterSecond
         };
         // 二级目录属性对象
         this.secondDir = {
             currIndex: 0, // 当前目录索引
-            dirNode: document.querySelector(option.secondDirNode),// 二级目录对应文本元素节点
+            contentNode: '',// 二级目录对应文本元素节点
         };
         this.route = useRoute();
         this.id = '';
+        if (option.container && option.firstDirNode) {
+            this.container = document.querySelector(option.container); // 目录文本容器
+            this.firstDir.contentNode = document.getElementsByTagName(option.firstDirNode) || ''
+        } else {
+            throw new Error('firstDirNode and container(content) is must')
+        }
+        this.secondDir.contentNode = option.secondDirNode || '';
+        // this.observe().then((res) => {
+        //    thid.directory.value = res;
+        // })
 
     }
-    // 离开二级目录
-    leaveSecond(directory, index, callback) {
-        if (directory[index].childDir.length != 0) {
-            directory[index].childDir[this.secondDir.currIndex].dirActive = false;
-        }
-        (callback && this.isSetHash) && callback(directory);
-    };
-    // 进入二级目录
-    enterSecond(directory, index, callback) {
-        if (directory[index].childDir.length != 0) {
-            directory[index].childDir[this.secondDir.currIndex].dirActive = true;
-        }
-        (callback && this.isSetHash) && callback(directory);
-    }
+
     // 设置hash
     setHash(dir) {
-        if (!id || id !== dir.find(item => item.dirActive)?.id) {
-            id = dir.find(item => item.dirActive)?.id;
-            location.href = location.href.replace(/(\.html)(#\w+|$)$/, `$1${id ? ('#' + id) : ''}`);
+        console.log(this);
+        if (!this.id || this.id !== dir.find(item => item.dirActive)?.id) {
+            this.id = dir.find(item => item.dirActive)?.id;
+            location.href = location.href.replace(/(\.html)(#\w+|$)$/, `$1${this.id ? ('#' + this.id) : ''}`);
         }
     }
-    scrollToHash() {
+    scrollToHash(offset) {
         let raf; // 请求动画帧
-        const offsetTop = this.route.hash && document.querySelector(this.route.hash).offsetTop - 80;
+        const offsetTop = (this.route.hash && document.querySelector(this.route.hash).offsetTop - 80) || offset;
         const fn = () => {
             const scrollTop = this.container.scrollTop;
             const isspeed = offsetTop / 8;
@@ -67,21 +78,31 @@ class SetDirectory {
 
     // 设置本页目录
     setDirNode() {
-        const dirNode = setArray(this.firstDir.dirNode);//一级标题node
+        let _this = this;
+        const dirNode = setArray(this.firstDir.contentNode);//一级标题node
         this.directory.value = dirNode.map(item => {
+            item.onclick = function () {
+                console.log(333);
+                _this.scrollToHash(item.offsetTop);
+            };
             const childDir = [];
-            setArray(item.parentNode.getElementsByTagName('h4')).map((node, index) => {
-                childDir.push({
-                    name: node.innerText,
-                    offsetTop: node.offsetTop,
+            if (this.secondDir.contentNode) {
+                setArray(item.parentNode.getElementsByTagName(this.secondDir.contentNode)).map((node, index) => {
+                    node.onclick = function () {
+
+                    };
+                    childDir.push({
+                        name: node.innerText,
+                        offsetTop: node.offsetTop,
+                    })
                 })
-            })
+            }
             return {
                 name: item.innerHTML,
                 id: item.id,
                 offsetTop: item.offsetTop,
                 dirActive: item.id == this.route.hash.replace('#', ''),
-                childDir: childDir || ''
+                childDir
             }
         });
         console.log(this.directory);
@@ -89,7 +110,7 @@ class SetDirectory {
 
     };
 
-    // 监听目录文本容器内容的变化设置当前页目录
+    // 监听目录文本容器内容的变化返回当前页目录
     observe() {
         return new Promise((resolve, reject) => {
             // 监听文本容器
@@ -109,9 +130,9 @@ class SetDirectory {
      * @param {Object} startDir 运行的目录对象
      */
     scrollDir(e, dir, startDir) {
-        dir = (dir || this.directory.value);
-        console.log(dir);
-        if (!dir || dir.length == 0) return
+        // dir = (dir || this.directory.value);
+
+        if (dir.length == 0) return
         startDir = (startDir || this.firstDir)
         const { currIndex, leaveSecond, enterSecond } = startDir;
         if (!e.target.scrollTop) {
@@ -119,26 +140,30 @@ class SetDirectory {
         }
         if (e.target.scrollTop > dir[currIndex].offsetTop - 100) {
             dir[0].dirActive = !currIndex;
+            // 开始滚动二级目录
+
             // 滚动到下一个目录
             if ((currIndex < dir.length - 1) && (e.target.scrollTop > dir[currIndex + 1].offsetTop - 100)) {
-                // leaveSecond && leaveSecond(dir, currIndex, this.setHash); // 离开二级目录
+                leaveSecond && leaveSecond(dir, currIndex); // 离开二级目录
                 startDir.currIndex++;
                 dir[currIndex + 1].dirActive = true;
                 dir[currIndex].dirActive = false;
-
+                this.setHash(dir);
             }
         } else {
             if (currIndex) {
                 dir[currIndex - 1].dirActive = true;
                 dir[currIndex].dirActive = false;
                 startDir.currIndex--;
-                // enterSecond && enterSecond(dir, startDir.currIndex, this.setHash);
+                enterSecond && enterSecond(dir, startDir.currIndex);
+                this.setHash(dir);
             } else {
                 dir[currIndex].dirActive = false;
             }
         }
-        // 开始滚动二级目录
-        // this.scrollDir(e,);
+        // 滚动二级目录
+        dir[currIndex].childDir && this.scrollDir(e, dir[currIndex].childDir, this.secondDir);
+
     };
 };
 
